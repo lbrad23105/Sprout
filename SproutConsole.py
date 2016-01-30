d95bd9779329854e9cf14ed0b158885e823f39e0
ScarletFloppy <tabletopwheelie@gmail.com>
2016-01-29 20:15:23 -0500

    Idk what to summary this, more in the description
    Added SproutConsole.html because its complicated to add it inline in the
    py file
    
    Modified SproutConsole.py to load the page properly because it needed to
    include socket, uncomment init call and change the port number because
    windows won't run it on 5207 for some reason. I left that line in there
    commented and changed it to port 80 for testing. I also modified it to
    read SproutConsole.html and serve it up properly (it can't be served as
    a string, it needs to be a bit-like value (error i got when trying to
    run before without encoding)) I also got many errors on missing
    parenthesis with print lines so I added parenthesis.
    
    I also modified the text that was the html inside of the
    SproutConsole.html so the tabs could be clicked on and have it generate
    the new page content in the white space on the right, however, when
    clicking on the actual tabs on the left nothing happens. If you call the
    functions I created though, they work. Also if we add jquery we can
    modify the write function to be jquery based which I am a fan of but it
    doesn't have to be done that way.  I was going to try to build out a
    Nodes page, but I haven't done that yet.

--- SproutConsole.py
+++ SproutConsole.py
@@ -1,220 +1,24 @@
- def __init__():
-        HOST, PORT = '', 5207
+import socket
 
-        sprout_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sprout_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sprout_socket.bind((HOST, PORT))
-        sprout_socket.listen(1)
-        print 'Sprout Console served on port %s ...' % PORT
-        while True:
-            client_connection, client_address = sprout_socket.accept()
-            request = client_connection.recv(1024)
-            print request
-            
-            # Required header that tells the browser how to render the text.
-            print("Content-Type: text/html\n\n")  # here text -- not html
+def __init__():
+    #HOST, PORT = '', 5207
+    RUN, HOST, PORT = True,'', 80
+    sprout_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sprout_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sprout_socket.bind((HOST, PORT))
+    sprout_socket.listen(1)
+    with open('SproutConsole.html', 'r') as SproutConsoleHtmlFile:
+        SproutConsoleHtml=SproutConsoleHtmlFile.read().replace('\n', '')
+    print('Sprout Console served on port %s ...' % PORT)
+    while (RUN == True):
+        client_connection, client_address = sprout_socket.accept()
+        request = client_connection.recv(1024)
+        print(request)
+        
+        # Required header that tells the browser how to render the text.
+        print("Content-Type: text/html\n\n")  # here text -- not html
 
-            page = ('''
-                <!DOCTYPE html>
-                <html lang = "en">
-                
-                <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
-                <!---https://coolors.co/3d315b-444b6e-708b75-9ab87a-f8f991 COlors-->
-                <style>
-                body
-                    {
-                            background-color: #FFFFFF;
-                            margin: 0px;
-                    }
+        client_connection.sendall(str.encode(SproutConsoleHtml))
+        client_connection.close()
 
-                    .header-bottom
-                    {
-                            width:100%;
-                            position:fixed;
-                            margin: auto;
-                            height: 7%;
-                            background-color: #004D40;
-                            float:left;
-                            top:93%;
-                            font-family: 'Open Sans';
-                            font-size: 10px;
-                            padding-left:2%;
-                    }
-
-                    .header-side
-                    {
-                            width:15%;
-                            position:fixed;
-                            margin: auto;
-                            height: 100%;
-                            background-color:#26A69A;
-                            float:left;
-                    }
-
-                    .header-config
-                    {
-                            width:25%;
-                            position:fixed;
-                            margin: auto;
-                            height: 100%;
-                            background-color: 
-                            
-                    }
-                    a{text-decoration: none; color: black;}
-                    .link
-                    {
-                            width: 100%;
-                            height: 5%;
-                            padding-top:10%;
-                            padding-left: 25%;
-                            font-family: Arial;
-                            text-decoration: none;
-                            font-size: 15px;
-                            float: center;
-                    }
-
-                       .link-active
-                    {
-                            width: 100%;
-                            height: 5%;
-                            padding-top:10%;
-                            padding-left: 25%;
-                            font-family: Arial;
-                            text-decoration: none;
-                            font-size: 15px;
-                            float: center;
-                            background-color:white;
-                    }
-
-                    .link:hover
-                    {
-                            width: 80%;
-                            height: 5%;
-                            padding-top:10%;
-                            padding-left: 25%;
-                            font-family: Arial;
-                            text-decoration: none;
-                            font-size: 15px;
-                            float: center;
-                            background-color: white;
-                    }
-
-                    .logolink
-                    {
-                            width: 85%;
-                            height: 10%;
-                            padding-top:15%;
-                            padding-left: 15%;
-                            font-family: Arial;
-                            text-decoration: none;
-                            font-size: 25px;
-                            float: center;
-                    }
-
-
-                    .logolink:hover
-                    {
-                            width: 85%;
-                            height: 10%;
-                            padding-top:15%;
-                            padding-left: 15%;
-                            font-family: Arial;
-                            text-decoration: none;
-                            font-size: 25px;
-                            float: center;
-                            background-color: #004D40;
-                            
-                    }
-
-                    .management-block
-                    {
-                            left:17%;
-                            float:center;
-                            position: absolute;
-                    }
-
-                    .management-block
-                    {
-                            width: relative;
-                            font-family: 'Open Sans',sans-serif ;
-                            font-weight: 300;
-                            padding: 3px 10px 10px 10px;
-                            height: relative;
-                            position: absolute;
-                            float: center;
-                            margin: auto;
-                    }
-
-
-
-                </style>
-                <title> Sprout Console </title>
-                </head>
-
-                <body style = "margin:0px">
-
-                        <div class = "main">
-                        <nav class = "header-side">
-                                
-                                <a href ="">
-                                <div class = "logolink">
-                                <img src = "http://i.imgur.com/7XA8xpe.png" width = "75%"/>
-                                </div></a>
-                                
-                                <a href ="">
-                                <div class = "link-active" id = "console">
-                                Console
-                                </div></a>
-                                
-                                <a href ="">
-                                <div class = "link" id = "nodes">
-                                Nodes
-                                </div></a>
-                                
-                                <a href ="">
-                                <div class = "link" id = "state">
-                                State
-                                </div></a>
-                                
-                        </nav>
-                        </div>
-                        
-                        <div class = "management">
-                                <div class = "management-block">
-                                        <h1> Welcome to Console </h1>
-                                        <h5 style = "padding-left: 5%";> > Get Started by Adding a Node </h5>
-                                        <h5 style = "padding-left: 5%";> > Then Configure it with SproutDSC </h5>
-                                        <h5 style = "padding-left: 5%";> > Finally, Check the State of Your Newly Configured Node </h5>
-                                        <h5 style = "padding-left: 5%";> > Example: Create a new file called foo.ps1 on a Windows System</h5>
-                                        <pre align = "left">
-                                        <code style = "font-size:15px">                                       
-    Node("foo.foobar.com",[ #In SproutDSC, all configurations are stored in an array.
-        File(
-        name = "foo.ps1",
-        content = "Get-Command",
-        ensure = "present",
-        path = "C:\\Users\\",
-        )# , Needed to seperate configurations.
-    ])
-                                        </code>
-                                        </pre>
-                                                 
-                                </div>
-                        
-                        
-                                <div class = "management-block">
-                                        
-                                </div>
-                        </div>
-                                
-                        <div class = "header-bottom">
-                        <a href = ""><h1>+NEW</h1></a>
-                        </div>
-                </body>
-                </html>
-
-            ''')
-    
-            client_connection.sendall(page)
-            client_connection.close()
-    #__init__()
+__init__()
